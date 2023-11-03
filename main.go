b0336ff655dcb7ea4d2c91323004fb94e2891980
bwz1984 <61036961+bwz1984@users.noreply.github.com>
2023-11-03 08:43:46 +0000

    fix:生产者-消费者模型

--- main.go
+++ main.go
@@ -1,7 +1,77 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"context"
+	"log"
+	"os"
+	"sync"
+)
+
+func (mfs *mergeFileStruct) producer(ctx context.Context, srcFileNames []string) {
+	wg := mfs.wg
+	lineChan := mfs.lineChan
+	wg.Add(len(srcFileNames))
+	for _, srcFileName := range srcFileNames {
+		go func(fileName string) {
+			defer wg.Done()
+			file, err := os.Open(fileName) // 打开文件
+			if err != nil {
+				log.Printf("os.Open %v fail, err:%v", fileName, err)
+				return
+			}
+			defer file.Close()
+			scanner := bufio.NewScanner(file)
+			for scanner.Scan() {
+				lineChan <- scanner.Text() // 按行写入
+			}
+		}(srcFileName)
+	}
+}
+
+func (mfs *mergeFileStruct) consumer(ctx context.Context, dstFileName string) {
+	wg := mfs.wg
+	lineChan := mfs.lineChan
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		dstFile, err := os.Create(dstFileName)
+		if err != nil {
+			log.Printf("os.Create %v fail, err:%v", dstFileName, err)
+			return
+		}
+		defer dstFile.Close()
+		for {
+			lineStr, ok := <-lineChan
+			if !ok {
+				break
+			}
+			dstFile.WriteString(lineStr + "\n")
+		}
+	}()
+}
+
+type mergeFileStruct struct {
+	wg       *sync.WaitGroup // 阻塞主进程 等待所有goroutine退出
+	lineChan chan string     // 传递文件行数据
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	ctx := context.TODO()
+	dstFileName := ""
+	srcFileNames := []string{}
+
+	wg := &sync.WaitGroup{}
+	lineChan := make(chan string, 1024*256) // 设置缓冲区大小 需要斟酌 保证一直有剩余空间可写
+
+	mfs := &mergeFileStruct{
+		wg:       wg,
+		lineChan: lineChan,
+	}
+
+	mfs.consumer(ctx, dstFileName)
+
+	mfs.producer(ctx, srcFileNames)
+
+	wg.Wait()
 }
